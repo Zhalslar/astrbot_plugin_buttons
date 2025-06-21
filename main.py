cb2b44189b00ecb5a12ec9de4b0a0ec96ce12ce1
Zhalslar <zsl2936169201@gmail.com>
2025-06-22 01:12:05 +0800

    优化

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ from .core.button import ButtonBuilder
 from .core.storage import ButtonStorage
 from astrbot import logger
 
-
 @register(
     "astrbot_plugin_buttons",
     "Zhalslar",
@@ -28,6 +27,19 @@ class ButtonPlugin(Star):
         buttons_data_file = os.path.join(plugin_data_dir, "buttons_data.json")
         self.storage = ButtonStorage(buttons_data_file)
         self.builder = ButtonBuilder(config)  # type: ignore
+
+    @filter.command("按钮", alias={"button", "bt"})
+    async def button_command(
+        self, event: AiocqhttpMessageEvent, label="点击", callback="我是笨蛋"
+    ):
+        "发送一个简单的回调按钮"
+        await self.send_callback_button(
+            client=event.bot,
+            buttons={label: callback},
+            group_id=int(event.get_group_id()),
+            user_id=int(event.get_sender_id()),
+        )
+        event.stop_event()
 
     @filter.event_message_type(filter.EventMessageType.ALL)
     async def on_message(self, event: AiocqhttpMessageEvent):
